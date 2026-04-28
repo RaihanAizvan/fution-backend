1a6a16d1f107848f23461956dd2f24e7ac05ce61
Mohammed Raihan <aizvanr@gmail.com>
2026-04-28 18:01:03 +0530

    refactor(blocks): delete legacy block-based content logic and DTOs

--- src/blocks/block-type.enum.ts
+++
@@ -1,8 +0,0 @@
-export enum BlockType {
-  INTRO = 'intro',
-  CODE = 'code',
-  ACCORDION = 'accordion',
-  CHECKLIST = 'checklist',
-  PITFALLS = 'pitfalls',
-  RESOURCES = 'resources'
-}
--- src/blocks/dto/code-block.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString } from 'class-validator';
-
-export class CodeBlockDto {
-  @IsString()
-  language: string;
-
-  @IsString()
-  code: string;
-}

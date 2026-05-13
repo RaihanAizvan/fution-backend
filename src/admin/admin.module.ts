5b6b10614662431e82817f78edf325e86eccd54e
Mohammed Raihan <aizvanr@gmail.com>
2026-05-13 20:29:27 +0530

    feat: wire admin auth module

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,4 +1,5 @@
-import { Module } from '@nestjs/common';
+import { AdminMiddleware } from './admin.middleware';
+import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { AdminSubjectsController } from './subjects/admin-subjects.controller';
 import { AdminSubjectsService } from './subjects/admin-subjects.service';
@@ -7,20 +8,52 @@ import { AdminTopicsRootController } from './topics/admin-topics-root.controller
 import { AdminTopicsService } from './topics/admin-topics.service';
 import { AdminTopicVersionsController } from './topics/admin-topic-versions.controller';
 import { AdminTopicVersionsService } from './topics/admin-topic-versions.service';
+import { AdminAuthController } from './auth/admin-auth.controller';
+import { AdminAuthService } from './auth/admin-auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1d' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [
+    AdminAuthController,
     AdminSubjectsController,
     AdminTopicsController,
     AdminTopicsRootController,
     AdminTopicVersionsController,
   ],
   providers: [
+    AdminAuthService,
     AdminSubjectsService,
     AdminTopicsService,
     AdminTopicVersionsService,
   ],
 })
-export class AdminModule { }
+export class AdminModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AdminMiddleware)
+      .exclude(
+        { path: 'admin/auth/login', method: RequestMethod.POST },
+        { path: 'admin/auth/request', method: RequestMethod.POST },
+      )
+      .forRoutes(
+        AdminSubjectsController,
+        AdminTopicsController,
+        AdminTopicsRootController,
+        AdminTopicVersionsController,
+        AdminAuthController,
+      );
+  }
+}
 
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SubjectsModule } from './subjects/subjects.module';
 import { TopicsModule } from './topics/topics.module';
 import { AdminModule } from './admin/admin.module';
 
-
 @Module({
-  imports: [SubjectsModule, TopicsModule, AdminModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    SubjectsModule,
+    TopicsModule,
+    AdminModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule { }
-
